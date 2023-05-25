3b7a9212f761489e5f012e145d118a29093b3b62
Julian Dunskus <julian.dunskus@gmail.com>
2023-05-25 16:50:47 +0200

    use new spray IDs

--- Sources/ValorantAPI/Models/IDs/SprayID.swift
+++ Sources/ValorantAPI/Models/IDs/SprayID.swift
@@ -9,10 +9,11 @@ public enum Spray {
 }
 
 public extension Spray.Slot.ID {
-	static let preRound = Self("0814b2fe-4512-60a4-5288-1fbdcec6ca48")!
-	static let midRound = Self("04af080a-4071-487b-61c0-5b9c0cfaac74")!
-	static let postRound = Self("5863985e-43ac-b05d-cb2d-139e72970014")!
+	static let right = Self("5863985e-43ac-b05d-cb2d-139e72970014")!
+	static let top = Self("7cdc908e-4f69-9140-a604-899bd879eed1")!
+	static let left = Self("0814b2fe-4512-60a4-5288-1fbdcec6ca48")!
+	static let bottom = Self("04af080a-4071-487b-61c0-5b9c0cfaac74")!
 	
-	/// all three spray slots in order (pre-, mid-, post-round)
-	static let inOrder = [preRound, midRound, postRound]
+	/// lists all 4 slots in counterclockwise order, starting from the right one (angle 0° by convention)
+	static let inCCWOrder = [right, top, left, bottom]
 }
